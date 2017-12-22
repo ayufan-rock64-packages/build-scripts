e0a297d319983399bb5fb5ad888893a0e8952907
Kamil Trzciński <ayufan@ayufan.eu>
2017-12-22 09:58:23 +0100

    Improve release.bash script

--- release.bash
+++ release.bash
@@ -1,26 +1,48 @@
 #!/bin/bash
 
 if [[ $# -ne 2 ]]; then
-  echo "usage: $0 package-name distribution"
+  echo "usage: $0 package-directory distribution"
   exit 1
 fi
 
-set -xeo pipefail
+set -eo pipefail
 
-SOURCENAME="$1"
+PACKAGE_DIR="$1"
 DISTRIBUTION="$2"
 
-if [[ ! -d "$SOURCENAME" ]]; then
-  echo "missing $SOURCENAME package"
+echo "Entering $PACKAGE_DIR..."
+cd "$PACKAGE_DIR"
+
+SOURCE=$(dpkg-parsechangelog -S Source)
+VERSION=$(dpkg-parsechangelog -S Version)
+TAG_NAME="ayufan/rock64-ppa/${VERSION}/${DISTRIBUTION}"
+
+echo "Building package '${SOURCE}' and '${VERSION}'..."
+
+PACKAGE_FILE="../${SOURCE}_${VERSION}"
+
+if ! git diff-files --quiet; then
+  echo "Dirty repository. Commit changes!"
   exit 1
 fi
 
-rm -f "${SOURCENAME}"_*.*
+echo "Cleaning previous packages..."
+rm -f "${PACKAGE_FILE}.dsc" "${PACKAGE_FILE}_source.*"
 
-pushd "$SOURCENAME"
+echo "Creating tag '${TAG_NAME}'..."
+git tag -a -m "${dpkg-parsechangelog}" "${TAG_NAME}"
+trap 'git tag -d "${TAG_NAME}"' EXIT
+
+echo "Building package for '${DISTRIBUTION}'..."
 debuild -S -sa -kB11A62DE -d -I.git \
   --changes-option=-Ddistribution="${DISTRIBUTION}" \
   --release-by="Kamil Trzcinski <ayufan@ayufan.eu>"
-popd
 
-dput ppa:ayufan/rock64-ppa "${SOURCENAME}"_*.changes
+echo "Uploading package '${PACKAGE_FILE}_source.changes'..."
+dput ppa:ayufan/rock64-ppa "${PACKAGE_FILE}_source.changes"
+
+echo "Uploading tag '${TAG_NAME}'..."
+git push ayufan "${TAG_NAME}" || git push origin "${TAG_NAME}"
+trap - EXIT
+
+echo "Done."
